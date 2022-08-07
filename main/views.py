bb191ec3d7c4ad278d31a8b755ff7a9ecd3689ea
lsw725 <725lsw@hanmail.net>
2022-08-07 23:28:20 +0900

    implement main page's url mapping

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "home.test.html")
+
+def guide(request):
+    return render(request, "guide.test.html")
