42ea857976c42454a27b2a0d8eec61cb98fb464a
lsw725 <725lsw@hanmail.net>
2022-08-07 22:56:16 +0900

    add main app

--- haehoo/settings.py
+++ haehoo/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "main.apps.MainConfig",
     "account.apps.AccountConfig",
 ]
 
