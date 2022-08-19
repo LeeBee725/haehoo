d0420e0078541eabaf0c6a462e3e6d8a2e7528cc
KIMJINBBANG <0925taekwonjy@gmail.com>
2022-08-20 00:09:52 +0900

    버킷리스트 READ 동작 구현 중

--- bucket_list/admin.py
+++ bucket_list/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Bucket
+from .models import CommentBucket
+from .models import Process
+
 # Register your models here.
--- bucket_list/views.py
+++ bucket_list/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Bucket
+from .models import CommentBucket
+from .models import Process
+
 def total(request):
     return render(request, "total.html")
 
@@ -8,3 +12,10 @@ def private(request, nickname):
 
 def create(request, nickname):
     return render(request, "create.test.html", {"nickname" : nickname})
+
+
+# models 담당
+
+def home(request):
+    home = home.objects
+    return render(request, 'total.test.html', {'home' : home})
