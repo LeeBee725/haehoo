42c3271ab96b03aafd65bae1ff68d6914e302c59
en <highjuno@gmail.com>
2022-08-20 04:36:28 +0900

    implement display private buckets

--- bucket_list/views.py
+++ bucket_list/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from account.models import HaehooUser
+from .models import Bucket
 
 def total(request):
     return render(request, "total.html")
 
 def private(request, nickname):
-    return render(request, "private.test.html", {"nickname" : nickname})
+    user = HaehooUser.objects.filter(nickname = nickname)
+    buckets = Bucket.objects.filter(userID = user.get())
+    return render(request, "private.test.html", {"nickname" : nickname, "buckets" : buckets})
 
 def create(request, nickname):
     if request.method == "POST":
