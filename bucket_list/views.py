12fc02055b48fcd740d893f95e06693f54281ee4
lsw725 <725lsw@hanmail.net>
2022-08-20 04:08:57 +0900

    implement Bucket CREATE

--- bucket_list/views.py
+++ bucket_list/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 
+from account.models import HaehooUser
+from bucket_list.models import Bucket
+
 def total(request):
     return render(request, "total.html")
 
@@ -8,6 +11,15 @@ def private(request, nickname):
 
 def create(request, nickname):
     if request.method == "POST":
+        user = HaehooUser.objects.filter(nickname=nickname)
+        title = request.POST["title"]
+        category = int(request.POST["category"])
+        newBucket = Bucket(
+            title = title,
+            category = category
+        )
+        newBucket.userID = user.get()
+        newBucket.save()
         return redirect('private', nickname=nickname)
     else:
         return render(request, "create.test.html", {"nickname" : nickname})
