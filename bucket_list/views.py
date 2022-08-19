734d66f7f1e290be4cb5ccb1c9441584e41f58ad
KIMJINBBANG <0925taekwonjy@gmail.com>
2022-08-20 03:35:44 +0900

    버킷리스트 READ 동작 구현 중

--- bucket_list/views.py
+++ bucket_list/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
-# from .models import Bucket
+from .models import Bucket
 # from .models import CommentBucket
 # from .models import Process
 
 def total(request):
-    # total_bucket = Bucket.objects
-    # return render(request, 'total.test.html', {'total_bucket' : total_bucket})
-    return render(request, "total.html")
+    total_bucket = Bucket.objects
+    return render(request, 'total.test.html', {'total_bucket' : total_bucket})
+    # return render(request, "total.html")
 
 def private(request, nickname):
     return render(request, "private.test.html", {"nickname" : nickname})
