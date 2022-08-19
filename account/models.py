3a0b12ad6dbfaa2ee4597af649753e2cd3c40ccf
en <highjuno@gmail.com>
2022-08-19 16:56:13 +0900

    created User model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    #ID : AbstractUSer의 내장 property(username)
+    #비밀번호 : AbstractUSer의 내장 property(password)
+    nickname = models.CharField(max_length = 10)
