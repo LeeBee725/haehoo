2a630a9382b9b21f9b73ccdda7267891daced2dd
KIMJINBBANG <0925taekwonjy@gmail.com>
2023-01-06 15:30:25 +0900

    add like and reference

--- bucket_list/models.py
+++ bucket_list/models.py
@@ -4,7 +4,9 @@ from account.models import HaehooUser
 # Create your models here.
 
 class Bucket(models.Model):
-    userID = models.ForeignKey(HaehooUser, on_delete = models.CASCADE)
+    userID = models.ForeignKey(HaehooUser, on_delete = models.CASCADE, related_name="user-make-bucket+")
     title = models.CharField(max_length = 100)
     category = models.IntegerField()
     createdAt = models.DateTimeField(auto_now = True)
+    liked_users = models.ManyToManyField(HaehooUser, blank = True, related_name="user-like-bucket+")
+    referenced_users = models.ManyToManyField(HaehooUser, blank = True, related_name="user-reference-bucket+")
