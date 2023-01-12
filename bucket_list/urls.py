319c6bd9ed321a0fed1cfcdcd88b630b6eca4f6b
KIMJINBBANG <0925taekwonjy@gmail.com>
2023-01-12 21:04:26 +0900

    implement edit function

--- bucket_list/urls.py
+++ bucket_list/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("<str:nickname>/", views.private, name="private"),
     path("<str:nickname>/create/", views.create, name="create"),
     path("<str:nickname>/delete/<int:bucket_id>", views.delete, name="delete"),
+    path("<str:nickname>/edit/<int:bucket_id>", views.edit, name="edit"),
+    path("<str:nickname>/update/<int:bucket_id>", views.update, name="update"),
 ]
