eb4b40df672a7ff578c1763d708b059ace68c2b2
KIMJINBBANG <0925taekwonjy@gmail.com>
2023-01-12 15:26:28 +0900

    testing

--- bucket_list/urls.py
+++ bucket_list/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("<str:nickname>/", views.private, name="private"),
     path("<str:nickname>/create/", views.create, name="create"),
     path("<str:nickname>/delete/<int:bucket_id>", views.delete, name="delete"),
+    path("<str:nickname>/private/<int:bucket_id>", views.private, name="private"),
+    path("<str:nickname>/edit/<int:bucket_id>", views.edit, name="edit"),
+    path("<str:nickname>/update/<int:bucket_id>", views.update, name="update"),
 ]
--- bucket_list/views.py
+++ bucket_list/views.py
@@ -31,3 +31,47 @@ def delete(request, nickname, bucket_id):
     bucket.delete()
 
     return redirect('private', nickname=nickname)
+
+def edit(request, nickname, bucket_id):
+    edit_bucket = Bucket.objects.get(id= bucket_id)
+    return render(request, 'edit.html', {"buckets" : edit_bucket})
+
+def update(request, nickname, bucket_id):
+    #user = HaehooUser.objects.filter(nickname = nickname)
+    # # update_bucket = Bucket.objects.filter(userID = user.get())
+    # # #updated_bucket = get_object_or_404(Bucket, pk = bucket_id)
+    # # update_bucket.title = request.POST["title"]
+    # # #update_bucket.writer = request.POST['writer']
+    # # #update_bucket.body = request.POST['body']
+    # # #update_bucket.pub_date = timezone.now()
+    # # update_bucket.save()
+
+    # update_bucket = get_object_or_404(Bucket, pk = bucket_id)
+    # update_bucket.title = request.POST.get('title')
+    # # bucket.content = request.POST.get('content')
+    # update_bucket.save()
+
+    
+    # return redirect('private', nickname = nickname)
+
+
+
+
+    # if request.method == "POST":
+    #     #user = HaehooUser.objects.filter(nickname=nickname)
+    #     #update_bucket = get_object_or_404(Bucket, pk = bucket_id)
+    #     update_bucket = Bucket.objects.get(bucket_id= bucket_id)
+    #     update_bucket.title = request.POST["title"]
+    #     update_bucket.save()
+
+    #     return redirect('private', nickname=nickname)
+    # else:
+    #     return render(request, "create.html", {"nickname" : nickname})
+
+    update_bucket = Bucket.objects.get(id= bucket_id)
+    update_bucket.title = request.POST.get("title") #,False
+    # buckets = Bucket.objects.filter(userID = user.get())
+    # update_bucket.pub_date = timezone.now()
+    update_bucket.save()
+
+    return redirect('private', update_bucket.id)
