02ca47bc182df748eec12e5e20e928e747c9f0d7
LeeBee <725lsw@hanmail.net>
2023-08-26 01:14:44 +0900

    feat: feedback 접수 기능 구현

--- customer_support/admin.py
+++ customer_support/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from customer_support.models import FeedBack
+
+admin.site.register(FeedBack)
--- customer_support/views.py
+++ customer_support/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from customer_support.models import FeedBack
+import time
 
 def feedback(request):
-    return render(request, 'feedback.html')
+    if request.method == "POST":
+        newFeedback = FeedBack(
+            content=request.POST["content"]
+        )
+        newFeedback.save()
+        time.sleep(1)
+        return redirect('main')
+    else:
+        return render(request, 'feedback.html')
